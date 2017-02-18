557b8392c9a10d4a45db462be976a5b3e221e01f
dhuang89 <dlh4fx@virginia.edu>
2017-02-18 15:03:00 -0500

    added save to text function

--- displayPage.php
+++ displayPage.php
@@ -8,26 +8,71 @@ Wyatt Tinsley wtj6tq -->
 </head>
 	
 <body><script src="functions.js" type="text/javascript"></script>
-<?php echo "<h1> Question Verification Page </h1>";
+<?php session_start();
+	  echo "<h1> Question Verification Page </h1>";
 	  echo "<h3> Review the entered information and submit when ready. </h3>"; 
 	  echo "<h3>Press back to return to previous page to edit question. </h3> <br> <br>";
+	  $GLOBALS['kind'] = 0;
 	  ?>
-Your entered question: <?php if (isset($_POST["question"])) { $type = "mul"; echo "<strong>" . $_POST["question"] . "</strong>";} ?> 
-<?php if (isset($_POST["question2"])) { $type = "tru"; echo "<strong>" . $_POST["question2"] . "</strong>";} ?>
-<?php if (isset($_POST["question3"])) { $type = "short"; echo "<strong>" . $_POST["question3"] . "</strong>";} ?> <br>
+Your entered question: <?php if (isset($_POST["question"])) { $type = "mul"; $_SESSION["type"] = 0; $_SESSION["q"] = $_POST["question"]; echo "<strong>" . $_POST["question"] . "</strong>";} ?> 
+<?php if (isset($_POST["question2"])) { $type = "tru"; $_SESSION["type"] = 1; $_SESSION["q"] = $_POST["question2"]; echo "<strong>" . $_POST["question2"] . "</strong>";} ?>
+<?php if (isset($_POST["question3"])) { $type = "short"; $_SESSION["type"] = 2; $_SESSION["q"] = $_POST["question3"]; echo "<strong>" . $_POST["question3"] . "</strong>";} ?> <br>
 
-<?php if (isset($_POST["Answer"])) { echo "Correct answer for multiple choice: <strong>" . $_POST["Answer"] . "</strong>";}?> 
-<?php if (isset($_POST["Answer2"])) { echo "Answer for true/false: <strong>" . $_POST["Answer2"] . "</strong>";} ?> 
-<?php if (isset($_POST["response"])) { echo "Answer for short answer: <strong>" . $_POST["response"] . "</strong>";} ?> <br>
+<?php if (isset($_POST["Answer"])) { $_SESSION["a"] = $_POST["Answer"]; echo "Correct answer for multiple choice: <strong>" . $_POST["Answer"] . "</strong>";}?> 
+<?php if (isset($_POST["Answer2"])) { $_SESSION["a"] = $_POST["Answer2"]; echo "Answer for true/false: <strong>" . $_POST["Answer2"] . "</strong>";} ?> 
+<?php if (isset($_POST["response"])) { $_SESSION["a"] = $_POST["response"]; echo "Answer for short answer: <strong>" . $_POST["response"] . "</strong>";} ?> <br>
 
 
-<?php if (isset($_POST["A"])) { echo "Answer A: <strong>" .  $_POST["A"] . "</strong>";} ?><br>
-<?php if (isset($_POST["B"])) { echo "Answer B: <strong>" .  $_POST["B"] . "</strong>";} ?><br>
-<?php if (isset($_POST["C"])) { echo "Answer C: <strong>" .  $_POST["C"] . "</strong>";} ?><br>
-<?php if (isset($_POST["D"])) { echo "Answer D: <strong>" .  $_POST["D"] . "</strong>";} ?><br>
+<?php if (isset($_POST["A"])) { $_SESSION["x"] = $_POST["A"]; echo "Answer A: <strong>" .  $_POST["A"] . "</strong>";} ?><br>
+<?php if (isset($_POST["B"])) { $_SESSION["y"] = $_POST["B"];echo "Answer B: <strong>" .  $_POST["B"] . "</strong>";} ?><br>
+<?php if (isset($_POST["C"])) { $_SESSION["z"] = $_POST["C"];echo "Answer C: <strong>" .  $_POST["C"] . "</strong>";} ?><br>
+<?php if (isset($_POST["D"])) { $_SESSION["zz"] = $_POST["D"];echo "Answer D: <strong>" .  $_POST["D"] . "</strong>";} ?><br>
 
 <input type="submit" name="back" value="Back" onClick="history.go(-1);"> 
-<input type="submit" name="confirm" value="Confirm">  
+
+<form method="post">
+	<input type="submit" name="confirm" id="confirm" value="Confirm">  
+</form>
+
+<form action="DataEntry.php">
+    <input type="submit" value="Create Another Question" />
+</form>
+
+<?php 
+
+function saveToText() {
+	$file = 'data.txt';
+	if ($_SESSION["type"] == 0) {
+		$question = "Question: " . $_SESSION["q"] . "\n";
+		$answer = "Answer: " . $_SESSION["a"] . "\n";
+		$a = "Choice A: " . $_SESSION["x"] . "\n";
+		$b = "Choice B: " . $_SESSION["y"] . "\n";
+		$c = "Choice C: " . $_SESSION["z"] . "\n";
+		$d = "Choice D: " . $_SESSION["zz"] . "\n";
+		file_put_contents($file, $question, FILE_APPEND | LOCK_EX);
+		file_put_contents($file, $answer, FILE_APPEND | LOCK_EX);
+		file_put_contents($file, $a, FILE_APPEND | LOCK_EX);
+		file_put_contents($file, $b, FILE_APPEND | LOCK_EX);
+		file_put_contents($file, $c, FILE_APPEND | LOCK_EX);
+		file_put_contents($file, $d, FILE_APPEND | LOCK_EX);
+	}
+
+	else {
+		$question = "Question: " . $_SESSION["q"] . "\n";
+		$answer = "Answer: " . $_SESSION["a"] . "\n";
+		file_put_contents($file, $question, FILE_APPEND | LOCK_EX);
+		file_put_contents($file, $answer, FILE_APPEND | LOCK_EX);
+	}
+	
+
+	echo "Question and answer saved to data file.";
+}
+
+if (array_key_exists('confirm', $_POST)) {
+	saveToText();
+}
+
+?>
 
 <script src="functions.js" type="text/javascript"></script>
 </body>
